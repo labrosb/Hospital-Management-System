569db08413a7153eb6ec652d1aae9ac1dde45213
labrosb <labros_b@htomail.com>
2016-09-23 16:29:51 +0300

    Update ALL

--- client_processes/new_exam_loader.js
+++
@@ -1,8 +0,0 @@
-
-	var first;
-	if (first != false){
-
-		$.getScript("client_processes/jquery/jquery.bpopup-0.7.0.min.js");
-
-	}
-	first=false;
--- content/manager/content.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo'
-		<div class="in_content logout_content"> </div>
-		<div class="in_content news_content"> </div>
-		<div class="in_content insert_staff_content"> </div>
-		<div class="in_content search_staff_content"> </div>
-		<div class="in_content contact_content"> </div>		';
-?>
