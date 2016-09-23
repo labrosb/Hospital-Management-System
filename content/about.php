0ee3506102033c1a0c9d10b45f56cda0840d35b2
labrosb <labros_b@htomail.com>
2016-09-23 16:35:50 +0300

    Guest section architecture

---
+++ content/about.php
@@ -0,0 +1,23 @@
+  
+ 		<div id="intro">
+						
+			<h1 data-inter="aboutUsTitle"></h1>
+						
+           <div class="content_page">
+            
+            	<div data-inter="aboutUsContent" id="left"> </div>
+				
+                <div id="right">
+                
+					<img src="styles/images/about_stock.jpg" alt="" />
+                                                    
+                </div><!--right ends-->
+            
+            </div><!--content end-->
+					
+						
+		</div><!--introduction end-->
+									
+		<script>
+			changeLang(defaultLang);
+		</script>	
