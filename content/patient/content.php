960870b11ef48f6e14206951f0c00e8b050aac51
labrosb <labros_b@htomail.com>
2016-09-23 16:36:17 +0300

    Patient section architecture

---
+++ content/patient/content.php
@@ -0,0 +1,14 @@
+<?php
+	echo'
+		<div class="in_content edit_profile_content"> </div>
+		<div class="in_content logout_content"> </div>
+		<div class="in_content news_content"> </div>
+		<div class="in_content exams_content"> </div>
+			<div class="in_content new_exam_content"> </div>
+			<div class="in_content exams_results_content"> </div>
+		<div class="in_content myHistory_content"> </div>
+			<div class="in_content exams_history_content"> </div>
+			<div class="in_content hosp_history_content"> </div>
+			<div class="in_content notif_history_content"> </div>
+		<div class="in_content contact_content"> </div>';
+?>
---
+++ content/patient/exams_results.php
@@ -0,0 +1,25 @@
+		<div id='height_specify'></div>
+		<div id="intro">
+						
+			<h1 data-inter='examsResultsTitle'>Examinations results</h1>
+			
+           <div class="content_page">
+            	<div id="center_table">               
+					</br></br>
+					<div id='results'></div>
+					<div id='more_results'>
+						<img id='loading_img' src='styles/images/loading2.gif' height='30' width='30' />
+					</div>           
+
+                </div>
+				
+            
+            </div><!--content end-->
+					
+						
+		</div><!--introduction end-->
+		
+	<script type="text/javascript" src="client_processes/exams_results.js"></script>
+	<script>
+		changeLang(defaultLang);
+	</script>
