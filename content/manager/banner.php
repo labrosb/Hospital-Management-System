56efcb6ec5fec80ccccb21e0fd0a275ac922db86
labrosb <labros_b@htomail.com>
2016-09-23 16:36:48 +0300

    Manager section architecture

---
+++ content/manager/banner.php
@@ -0,0 +1,20 @@
+		<div id ="banner_index">	
+			<div class="slider-wrapper theme-default">
+				<div id="slider" class="nivoSlider manager_slider">
+					<img src="styles/images/banner_pic.png" alt="" />
+					<img src="styles/images/banner_pic2.png" alt="" />
+					<img src="styles/images/banner_pic.png" alt="" />
+					<img src="styles/images/banner_pic2.png" alt="" />
+				</div>
+			</div>			
+			<div id ="banner_text">	
+				<h1 data-inter="managerBannerTitle"> Administrative Staff Area </h1>
+				<p data-inter="managerBannerText"> You are logged in as Administrative Staff .</br>
+					You can now access the daily and weekly schedule of medical staff and medical units.
+					You can also search, insert or delete medical staff if its required.
+				</p>
+			</div>
+		</div>		
+		<script type="text/javascript">
+			$(".manager_slider").nivoSlider();
+		</script>
